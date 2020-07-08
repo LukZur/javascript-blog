9ff858008e9e6949a21eb5ad4ea0c91e36b94a4a
Łukasz Żurawiecki <VonKranich46@gmail.com>
2020-07-08 14:49:24 +0200

    Added funcionality to add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@
   ("use strict");
 
   const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log("Link was clicked!");
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE]  remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll(".titles a.active");
 
@@ -15,9 +16,13 @@
 
     /* add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log("clickedElement:", clickedElement);
 
-    const activeArticles = document.querySelectorAll(".posts article.active");
+    clickedElement.classList.add("active");
+
+    /* [DONE]  remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll(".post article.active");
 
     for (let activeArticle of activeArticles) {
       activeArticle.classList.remove("active");
