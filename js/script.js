7d5f1d1a229079b835f268573197e33a06e5a9b1
Łukasz Żurawiecki <VonKranich46@gmail.com>
2020-07-10 13:18:47 +0200

    Added logic to script.js - whole script is working now

--- js/script.js
+++ js/script.js
@@ -46,9 +46,47 @@
     getArticle.classList.add("active");
   };
 
-  const links = document.querySelectorAll(".titles a");
+  const titleList = document.querySelector(".titles");
 
-  for (let link of links) {
-    link.addEventListener("click", titleClickHandler);
+  function clearTitleLinks(list) {
+    list.innerHTML = "";
   }
+
+  function generateTitleLinks() {
+    /* usuń zawartość listy linków w lewej kolumnie */
+
+    clearTitleLinks(titleList);
+
+    const articleList = document.querySelectorAll(".post");
+    let html = "";
+    /*następnie dla każdego artykułu: */
+    for (article of articleList) {
+      //   console.log(article);
+      /* odczytaj jego id i zapisz je do stałej*/
+      const articleID = article.getAttribute("id");
+      //   console.log(articleID);
+      /*znajdź element z tytułem i zapisz jego zawartość do stałej*/
+      const articleTitle = article.querySelector(".post-title").innerHTML;
+      //   console.log(articleTitle);
+      /*na podstawie tych informacji stwórz kod HTML linka i zapisz go do stałej*/
+      const linkCode =
+        '<li><a href="#' +
+        articleID +
+        '"><span>' +
+        articleTitle +
+        "</span></a></li>";
+      //   console.log(linkCode);
+
+      html = html + linkCode;
+      //   console.log(html);
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll(".titles a");
+
+    for (let link of links) {
+      link.addEventListener("click", titleClickHandler);
+    }
+  }
+  generateTitleLinks();
 }
